043c528e5c586b65258e7cf6adb822beecbe540a
Maria Vlodzianovska <ipz234@student.ztu.edu.ua>
2025-03-02 21:47:50 +0200

    added 3rd task of the 2nd lab

---
+++ lab-2/Singleton/Authenticator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Authenticator
+    {
+        private static readonly Lazy<Authenticator> instance = new Lazy<Authenticator>(() => new Authenticator());
+
+        private Authenticator()
+        {
+            Console.WriteLine("Authenticator instance created.");
+        }
+
+        public static Authenticator Instance => instance.Value;
+
+        public void Authenticate(string username)
+        {
+            Console.WriteLine($"User {username} authenticated.");
+        }
+    }
+}
---
+++ lab-2/Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using Singleton;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting the program...");
+        
+        Authenticator authenticator1 = Authenticator.Instance;
+        authenticator1.Authenticate("User1");
+
+        Authenticator authenticator2 = Authenticator.Instance;
+        authenticator2.Authenticate("User2");
+
+        if (authenticator1 == authenticator2)
+        {
+            Console.WriteLine("authenticator1 and authenticator2 are the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("authenticator1 and authenticator2 are different instances.");
+        }
+
+        Console.WriteLine("Program finished.");
+    }
+}
