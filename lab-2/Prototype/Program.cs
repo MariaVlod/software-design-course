8e58015ea200e73d032040d7e99dc00c82f07a94
Maria Vlodzianovska <ipz234@student.ztu.edu.ua>
2025-03-02 22:04:24 +0200

    added 4th task of the 2nd lab

---
+++ lab-2/Prototype/Program.cs
@@ -0,0 +1,37 @@
+﻿using Prototype;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Creating the virus family...");
+        Virus grandfather = new Virus(1.5, 10, "Grandfather Virus", "Alpha");
+        Virus father = new Virus(1.2, 5, "Father Virus", "Beta");
+        Virus son1 = new Virus(0.8, 2, "Son Virus 1", "Gamma");
+        Virus son2 = new Virus(0.9, 3, "Son Virus 2", "Gamma");
+
+        grandfather.AddChild(father);
+        father.AddChild(son1);
+        father.AddChild(son2);
+
+        Console.WriteLine("\nOriginal Virus Family:");
+        grandfather.DisplayInfo();
+        
+        Virus clonedFather = (Virus)father.Clone();
+        Console.WriteLine("\nCloned Virus Family:");
+        clonedFather.DisplayInfo();
+        
+        Console.WriteLine("\nModifying the cloned family...");
+        clonedFather.Name = "Cloned Father Virus";
+        clonedFather.Children[0].Name = "Cloned Son Virus 1";
+
+        Console.WriteLine("\nOriginal Virus Family After Modification:");
+        grandfather.DisplayInfo();
+
+        Console.WriteLine("\nCloned Virus Family After Modification:");
+        clonedFather.DisplayInfo();
+
+        Console.WriteLine("\nProgram finished.");
+    }
+}
---
+++ lab-2/Prototype/Virus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Virus : ICloneable
+    {
+        public double Weight { get; set; } 
+        public int Age { get; set; } 
+        public string Name { get; set; } 
+        public string Species { get; set; }
+        public List<Virus> Children { get; set; } 
+
+        public Virus(double weight, int age, string name, string species)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Species = species;
+            Children = new List<Virus>();
+        }
+
+        public void AddChild(Virus child)
+        {
+            Children.Add(child);
+        }
+
+       
+        public object Clone()
+        {
+            Virus clone = new Virus(Weight, Age, Name, Species);
+
+            foreach (var child in Children)
+            {
+                clone.AddChild((Virus)child.Clone()); 
+            }
+
+            return clone;
+        }
+        public void DisplayInfo(string prefix = "")
+        {
+            Console.WriteLine($"{prefix}Name: {Name}, Species: {Species}, Age: {Age}, Weight: {Weight}");
+            foreach (var child in Children)
+            {
+                child.DisplayInfo(prefix + "  "); 
+            }
+        }
+    }
+}
